f783379e0bc3501c43762b1ca4eeb2ebf4237eb5
kingson299 <86462000+kingson299@users.noreply.github.com>
2022-03-03 14:10:02 +0800

    update cors

--- app.js
+++ app.js
@@ -11,7 +11,18 @@ const auth = require("./middleware/auth");
 const app = express();
 
 app.use(express.json({ limit: "50mb" }));
-app.use(cors());
+
+const allowedOrigins = ['http://localhost:3000']
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not allow access';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.post("/register", async (req, res) => {
   try {
