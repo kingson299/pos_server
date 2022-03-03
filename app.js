1ccd8d439805c7b0e72361ab165fb32316097287
kingson299 <86462000+kingson299@users.noreply.github.com>
2022-03-03 14:22:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cors({
       return callback(new Error(msg), false);
     }
     return callback(null, true);
-  }
+  },credentials: true
 }));
 
 app.post("/register", async (req, res) => {
